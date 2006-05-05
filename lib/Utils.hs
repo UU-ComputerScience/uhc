4df328e1ea5898922e4546aa1e9a1277846ad11e
atzedijkstra <atzedijkstra@gmail.com>
2006-05-05 09:35:19 +0000

    work on ruler doc, grin codegen fix

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -9,13 +9,13 @@ import Debug.Trace
 -- Utils for tools
 -------------------------------------------------------------------------
 
-mkTexCmdDef :: (PP a, PP b) => String -> a -> b -> PP_Doc
-mkTexCmdDef cmd nm def = "\\" >|< cmd >|< "{" >|< pp nm >|< "}{%" >-< pp def >-< "}"
+mkTexCmdDef :: (PP cmd, PP a, PP b) => cmd -> a -> b -> PP_Doc
+mkTexCmdDef cmd nm def = "\\" >|< pp cmd >|< "{" >|< pp nm >|< "}{%" >-< pp def >-< "}"
 
-mkTexCmdUse :: PP a => String -> a -> PP_Doc
-mkTexCmdUse cmd nm = "\\" >|< cmd >|< "{" >|< pp nm >|< "}"
+mkTexCmdUse :: (PP cmd, PP a) => cmd -> a -> PP_Doc
+mkTexCmdUse cmd nm = "\\" >|< pp cmd >|< "{" >|< pp nm >|< "}"
 
-mkTexCmdUse' :: PP a => String -> a -> PP_Doc
+mkTexCmdUse' :: (PP cmd, PP a) => cmd -> a -> PP_Doc
 mkTexCmdUse' cmd nm = mkTexCmdUse cmd nm >|< "%"
 
 -------------------------------------------------------------------------
