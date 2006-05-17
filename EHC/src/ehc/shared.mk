3dc39193993366f7e8dfebe658c9f44416f6636d
atzedijkstra <atzedijkstra@gmail.com>
2006-05-17 15:40:05 +0000

    cygwin

--- EHC/src/ehc/shared.mk
+++ EHC/src/ehc/shared.mk
@@ -2,4 +2,4 @@
 SRC_EHC_PREFIX							:= $(SRC_PREFIX)ehc/
 SRC_EHC_RULES_PREFIX					:= $(SRC_EHC_PREFIX)rules/
 EHC_SUBDIRS								:= $(patsubst $(SRC_EHC_PREFIX)/%/,%,$(SRC_EHC_RULES_PREFIX))
-LATEX_EHC_SUBDIRS						:= $(patsubst $(SRC_EHC_PREFIX)%,%$(PATHS_SEP),$(EHC_SUBDIRS))
+LATEX_EHC_SUBDIRS						:= $(patsubst $(SRC_EHC_PREFIX)%,%$(PATHS_SEP_COL),$(EHC_SUBDIRS))
