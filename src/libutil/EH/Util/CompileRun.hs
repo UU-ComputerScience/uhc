49857fde0d86bde08bbf9a1b9986c58b34d1aecb
JeroenFokker <J.D.Fokker@uu.nl>
2006-07-07 14:55:51 +0000

    Monadified CompileRun utility library; Adapted EHC and Ruler main file to use it; Streamlined EHC main data flow; EHC can now also parse grin-files, making grinc obsolete

--- src/libutil/EH/Util/CompileRun.hs
+++ src/libutil/EH/Util/CompileRun.hs
@@ -5,6 +5,7 @@
 module EH.Util.CompileRun
   ( CompileRunState(..)
   , CompileRun(..)
+  , CompilePhase
   , CompileUnit(..)
   , CompileUnitState(..)
   , CompileRunError(..)
@@ -13,24 +14,22 @@ module EH.Util.CompileRun
   
   , mkEmptyCompileRun
 
-  , liftCR
-  , crCU, crMbCU, crUpdCU
-  , crSetFail, crSetErrs, crSetLimitErrs, crSetLimitErrsWhen, crSetInfos
-  , crHandle1, crSeq
-
-  , crCUState, crCUFPath
-  , crFindFileForFPath
-  
-  , crImportGather
-  
-  , crPP, crPPMsg
+  , crCU, crMbCU
   , ppCR
+  
+  , cpUpdCU
+  , cpSetFail, cpSetOk, cpSetErrs, cpSetLimitErrs, cpSetLimitErrsWhen, cpSetInfos, cpSetCompileOrder
+  , cpSeq
+  , cpFindFileForFPath
+  , cpImportGather
+  , cpPP, cpPPMsg
   )
   where
 
 import Maybe
 import System.Exit
 import Control.Monad
+import Control.Monad.State
 import IO
 import qualified Data.Map as Map
 import UU.Pretty
@@ -38,6 +37,7 @@ import UU.DData.Scc as Scc
 import EH.Util.Utils( panicJust )
 import EH.Util.PPUtils
 import EH.Util.FPath
+
 
 -------------------------------------------------------------------------
 -- Interfacing with actual state info
@@ -89,6 +89,10 @@ data CompileRun nm unit info err
       , crStateInfo     :: info
       }
 
+type CompilePhase n u i e a = StateT (CompileRun n u i e) IO a
+
+
+
 mkEmptyCompileRun :: n -> i -> CompileRun n u i e
 mkEmptyCompileRun nm info
   = CompileRun
@@ -116,81 +120,26 @@ crPP m cr = do { hPutStrLn stderr (m ++ ":") ; hPutPPLn stderr (ppCR cr) ; hFlus
 crPPMsg :: (PP m) => m -> CompileRun n u i e -> IO (CompileRun n u i e)
 crPPMsg m cr = do { hPutPPLn stdout (pp m) ; return cr }
 
+cpPP :: (PP n,PP u) => String -> CompilePhase n u i e ()
+cpPP m
+ = do { lift (hPutStrLn stderr (m ++ ":"))
+      ; cr <- get
+      ; lift (hPutPPLn stderr (ppCR cr))
+      ; lift (hFlush stderr)
+      ; return () 
+      }
+
+cpPPMsg :: (PP m) => m -> CompilePhase n u i e ()
+cpPPMsg m 
+ = do { lift (hPutPPLn stdout (pp m))
+      ; return ()
+      }
+
+
+
 -------------------------------------------------------------------------
 -- State manipulation, sequencing
 -------------------------------------------------------------------------
-
-liftCR :: (a -> b) -> a -> IO b
-liftCR f = \a -> return (f a)
-
-crHandle1 :: CompileRunError e p => (a -> IO b) -> (a -> b) -> (a -> CompileRunState e) -> IO a -> IO b
-crHandle1 action err errs it
-  = do { v <- it
-       ; case errs v of
-           CRSFailErrL about es (Just lim)
-             -> do { let (showErrs,omitErrs) = splitAt lim es
-                   ; unless (null about) (hPutPPLn stderr (pp about))
-                   ; putErr' (if null omitErrs then return () else hPutStrLn stderr "... and more errors") showErrs
-                   ; failOrNot es v
-                   }
-           CRSFailErrL about es Nothing
-             -> do { unless (null about) (hPutPPLn stderr (pp about))
-                   ; putErr' (return ()) es
-                   ; failOrNot es v
-                   }
-           CRSErrInfoL about doPrint is
-             -> do { if null is
-                     then return ()
-                     else do { hFlush stdout
-                             ; hPutPPLn stderr (about >#< "found errors" >-< e)
-                             }
-                   ; if not (null is) then return (err v) else action v
-                   }
-             where e = if doPrint then crePPErrL is else empty
-           CRSFail
-             -> do { exitFailure 
-                   ; return (err v)
-                   }
-           CRSOk
-             -> action v
-       }
-  where putErr' m e   = {- if null e
-                        then return ()
-                        else -}
-                             do { hPutPPLn stderr (crePPErrL e)
-                                ; m
-                                ; hFlush stderr
-                                }
-        failOrNot es v = if creAreFatal es then return (err v) else action v
-
-
-crSetFail :: CompileRun n u i e -> CompileRun n u i e
-crSetFail cr = cr {crState = CRSFail}
-
-crSetOk :: CompileRun n u i e -> CompileRun n u i e
-crSetOk cr = cr {crState = CRSOk}
-
-crSetErrs' :: Maybe Int -> String -> [e] -> CompileRun n u i e -> CompileRun n u i e
-crSetErrs' limit about es cr
-  = case es of
-      [] -> cr
-      _  -> cr {crState = CRSFailErrL about es limit}
-
-crSetErrs :: [e] -> CompileRun n u i e -> IO (CompileRun n u i e)
-crSetErrs e = liftCR (crSetErrs' Nothing "" e)
-
-crSetLimitErrs, crSetLimitErrsWhen :: Int -> String -> [e] -> CompileRun n u i e -> IO (CompileRun n u i e)
-crSetLimitErrs l a e = liftCR (crSetErrs' (Just l) a e)
-crSetLimitErrsWhen l a e cr = if null e then return cr else crSetLimitErrs l a e cr
-
-crSetInfos' :: String -> Bool -> [e] -> CompileRun n u i e -> CompileRun n u i e
-crSetInfos' msg dp is cr
-  = case is of
-      [] -> cr
-      _  -> cr {crState = CRSErrInfoL msg dp is}
-
-crSetInfos :: String -> Bool -> [e] -> CompileRun n u i e -> IO (CompileRun n u i e)
-crSetInfos msg dp is = liftCR (crSetInfos' msg dp is)
 
 crMbCU :: Ord n => n -> CompileRun n u i e -> Maybe u
 crMbCU modNm cr = Map.lookup modNm (crCUCache cr)
@@ -198,15 +147,20 @@ crMbCU modNm cr = Map.lookup modNm (crCUCache cr)
 crCU :: (Show n,Ord n) => n -> CompileRun n u i e -> u
 crCU modNm = panicJust ("crCU: " ++ show modNm) . crMbCU modNm
 
-crUpdCU :: (Ord n,CompileUnit u n s) => n -> (u -> IO u) -> CompileRun n u i e -> IO (CompileRun n u i e)
-crUpdCU modNm upd cr
-  = do { cu <- maybe (upd cuDefault) upd (crMbCU modNm cr)
-       ; return (cr {crCUCache = Map.insert modNm cu (crCUCache cr)})
-       }
+crSetFail :: CompileRun n u i e -> CompileRun n u i e
+crSetFail cr = cr {crState = CRSFail}
 
-crSeq :: CompileRunError e p => [CompileRun n u i e -> IO (CompileRun n u i e)] -> CompileRun n u i e -> IO (CompileRun n u i e)
-crSeq []      cr = return cr
-crSeq (a:as)  cr = crHandle1 (\cr -> crSeq as (crSetOk cr)) crSetFail crState (a cr)
+crSetErrs' :: Maybe Int -> String -> [e] -> CompileRun n u i e -> CompileRun n u i e
+crSetErrs' limit about es cr
+  = case es of
+      [] -> cr
+      _  -> cr {crState = CRSFailErrL about es limit}
+
+crSetInfos' :: String -> Bool -> [e] -> CompileRun n u i e -> CompileRun n u i e
+crSetInfos' msg dp is cr
+  = case is of
+      [] -> cr
+      _  -> cr {crState = CRSErrInfoL msg dp is}
 
 -------------------------------------------------------------------------
 -- Compile unit observations
@@ -222,49 +176,154 @@ crCUFPath modNm cr = maybe emptyFPath cuFPath (crMbCU modNm cr)
 -- Find file for FPath
 -------------------------------------------------------------------------
 
-crFindFileForFPath
+cpFindFileForFPath
   :: (Ord n,Show n,CompileUnitState s,CompileRunError e p,CompileUnit u n s,CompileModName n,CompileRunStateInfo i n p)
-       => Map.Map String s -> [String] -> Maybe n -> Maybe FPath -> CompileRun n u i e -> IO (CompileRun n u i e,Maybe FPath)
-crFindFileForFPath suffs sp mbModNm mbFp cr
-  = if cusIsUnk cus
-    then do { let fp = maybe (mkFPath $ show $ panicJust ("crFindFileForFPath") $ mbModNm) id mbFp
-                  modNm = maybe (mkCMNm $ fpathBase $ fp) id mbModNm
-            ; mbFpFound <- searchPathForReadableFile sp (Map.keys suffs) fp
-            ; case mbFpFound of
-                Nothing
-                  -> do { cr' <- crSetErrs (creMkNotFoundErrL (crsiImportPosOfCUKey modNm (crStateInfo cr)) (fpathToStr fp) sp) cr
-                        ; return (cr',Nothing)
-                        }
-                Just ff
-                  -> do { cr' <- crUpdCU modNm (\cu -> return (cuUpdFPath ff $ cuUpdState cus $ cuUpdKey modNm $ cu)) cr
-                        ; return (cr',Just ff)
-                        }
-                  where -- cus = Map.findWithDefault cusUnk (fpathSuff ff) suffs
-                        cus = case Map.lookup (fpathSuff ff) suffs of
-                                Just c  -> c
-                                Nothing -> case Map.lookup "*" suffs of
-                                             Just c  -> c
-                                             Nothing -> cusUnk
-                        
-            }
-    else return (cr,maybe Nothing (\nm -> Just (crCUFPath nm cr)) mbModNm)
-  where cus = maybe cusUnk (flip crCUState cr) mbModNm
+       => Map.Map String s -> [String] -> Maybe n -> Maybe FPath -> CompilePhase n u i e (Maybe FPath)
+cpFindFileForFPath suffs sp mbModNm mbFp
+  = do { cr <- get
+       ; let cus = maybe cusUnk (flip crCUState cr) mbModNm
+       ; if cusIsUnk cus
+          then do { let fp = maybe (mkFPath $ show $ panicJust ("cpFindFileForFPath") $ mbModNm) id mbFp
+                        modNm = maybe (mkCMNm $ fpathBase $ fp) id mbModNm
+                  ; mbFpFound <- lift (searchPathForReadableFile sp (Map.keys suffs) fp)
+                  ; case mbFpFound of
+                      Nothing
+                        -> do { cpSetErrs (creMkNotFoundErrL (crsiImportPosOfCUKey modNm (crStateInfo cr)) (fpathToStr fp) sp)
+                              ; return Nothing
+                              }
+                      Just ff
+                        -> do { cpUpdCU modNm (cuUpdFPath ff . cuUpdState cus . cuUpdKey modNm)
+                              ; return (Just ff)
+                              }
+                        where -- cus = Map.findWithDefault cusUnk (fpathSuff ff) suffs
+                              cus = case Map.lookup (fpathSuff ff) suffs of
+                                      Just c  -> c
+                                      Nothing -> case Map.lookup "*" suffs of
+                                                   Just c  -> c
+                                                   Nothing -> cusUnk
+
+                  }
+          else return (maybe Nothing (\nm -> Just (crCUFPath nm cr)) mbModNm)
+       }
 
 -------------------------------------------------------------------------
 -- Gather all imports
 -------------------------------------------------------------------------
 
-crImportGather
+cpImportGather
   :: (Show n,Ord n,CompileUnit u n s,CompileRunError e p,CompileUnitState s)
-       => (n -> CompileRun n u i e -> IO (CompileRun n u i e)) -> n -> CompileRun n u i e -> IO (CompileRun n u i e)
-crImportGather imp1Mod modNm
-  = crSeq [imp1Mod modNm, imps modNm, crImportScc]
-  where imps m cr
-          = crSeq (map (\n -> crSeq [imp1Mod n, imps n]) impL) cr
-          where impL = [ i | i <- cuImports (crCU m cr), not (cusIsImpKnown (crCUState i cr)) ]
+       => (n -> CompilePhase n u i e ()) -> n -> CompilePhase n u i e ()
+cpImportGather imp1Mod modNm
+  = do { cr <- get
+       ; let impL m = [ i | i <- cuImports (crCU m cr), not (cusIsImpKnown (crCUState i cr)) ]
+             imps m = cpSeq (map (\n -> cpSeq [imp1Mod n, imps n]) (impL m))
+       ; cpSeq [imp1Mod modNm, imps modNm, cpImportScc]
+       }
 
 crImportDepL :: (CompileUnit u n s) => CompileRun n u i e -> [(n,[n])]
 crImportDepL = map (\cu -> (cuKey cu,cuImports cu)) . Map.elems . crCUCache
 
-crImportScc :: (Ord n,CompileUnit u n s) => CompileRun n u i e -> IO (CompileRun n u i e)
-crImportScc cr = return (cr {crCompileOrder = Scc.scc (crImportDepL cr)})
+cpImportScc :: (Ord n,CompileUnit u n s) => CompilePhase n u i e ()
+cpImportScc = modify (\cr -> (cr {crCompileOrder = Scc.scc (crImportDepL cr)}))
+
+
+-------------------------------------------------------------------------
+-- State manipulation, sequencing (Monadic)
+-------------------------------------------------------------------------
+
+cpSetErrs :: [e] -> CompilePhase n u i e ()
+cpSetErrs es
+ = modify (crSetErrs' Nothing "" es)
+
+cpSetInfos :: String -> Bool -> [e] -> CompilePhase n u i e ()
+cpSetInfos msg dp is
+ = modify (crSetInfos' msg dp is)
+
+cpSetFail :: CompilePhase n u i e ()
+cpSetFail
+ = modify crSetFail
+
+cpSetOk :: CompilePhase n u i e ()
+cpSetOk
+ = modify (\cr -> (cr {crState = CRSOk}))
+
+cpSetCompileOrder :: [[n]] -> CompilePhase n u i e ()
+cpSetCompileOrder nameLL
+ = modify (\cr -> (cr {crCompileOrder = nameLL}))
+
+cpSetLimitErrs, cpSetLimitErrsWhen :: Int -> String -> [e] -> CompilePhase n u i e ()
+cpSetLimitErrs l a e
+ = modify (crSetErrs' (Just l) a e)
+
+cpSetLimitErrsWhen l a e
+ = do { when (not (null e))
+             (cpSetLimitErrs l a e)
+      }
+
+cpUpdCUM :: (Ord n,CompileUnit u n s) => n -> (u -> IO u) -> CompilePhase n u i e ()
+cpUpdCUM modNm upd
+  = do { cr <- get
+       ; cu <- lift (maybe (upd cuDefault) upd (crMbCU modNm cr))
+       ; put (cr {crCUCache = Map.insert modNm cu (crCUCache cr)})
+       }
+
+
+cpUpdCU :: (Ord n,CompileUnit u n s) => n -> (u -> u) -> CompilePhase n u i e ()
+cpUpdCU modNm upd
+  = do { cr <- get
+       ; let cu = (maybe (upd cuDefault) upd (crMbCU modNm cr))
+       ; put (cr {crCUCache = Map.insert modNm cu (crCUCache cr)})
+       }
+
+{-
+cpUpdCU modNm upd
+ = cpUpdCUM modNm (return . upd)
+-}
+
+cpSeq :: CompileRunError e p => [CompilePhase n u i e ()] -> CompilePhase n u i e ()
+cpSeq []     = return ()
+cpSeq (a:as) = cpHandle1 (do { cpSetOk
+                             ; cpSeq as
+                             })
+                         a
+
+cpHandle1 :: CompileRunError e p => CompilePhase n u i e () -> CompilePhase n u i e () -> CompilePhase n u i e ()
+cpHandle1 rest first
+  = do { _ <- first
+       ; cr <- get
+       ; case crState cr of
+           CRSFailErrL about es (Just lim)
+             -> do { let (showErrs,omitErrs) = splitAt lim es
+                   ; lift (unless (null about) (hPutPPLn stderr (pp about)))
+                   ; lift (putErr' (if null omitErrs then return () else hPutStrLn stderr "... and more errors") showErrs)
+                   ; failOrNot es
+                   }
+           CRSFailErrL about es Nothing
+             -> do { lift (unless (null about) (hPutPPLn stderr (pp about)))
+                   ; lift (putErr' (return ()) es)
+                   ; failOrNot es
+                   }
+           CRSErrInfoL about doPrint is
+             -> do { if null is
+                     then return ()
+                     else lift (do { hFlush stdout
+                                   ; hPutPPLn stderr (about >#< "found errors" >-< e)
+                                   })
+                   ; if not (null is) then cpSetFail else rest
+                   }
+             where e = empty -- if doPrint then crePPErrL is else empty
+           CRSFail
+             -> do { lift exitFailure
+                   ; cpSetFail
+                   }
+           CRSOk
+             -> rest
+       }
+  where putErr' m e   = if null e
+                        then return ()
+                        else
+                             do { hPutPPLn stderr (crePPErrL e)
+                                ; m
+                                ; hFlush stderr
+                                }
+        failOrNot es = if creAreFatal es then cpSetFail else rest
