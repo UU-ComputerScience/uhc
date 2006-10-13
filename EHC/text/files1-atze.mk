b9c83e3e967418e9248d3ff36e6c249e22527e1b
atzedijkstra <atzedijkstra@gmail.com>
2006-10-13 19:02:49 +0000

    esop07 submission

--- EHC/text/files1-atze.mk
+++ EHC/text/files1-atze.mk
@@ -2,11 +2,12 @@
 TEXT_PUB_VARIANTS			+= ruler-doc shuffle-doc ehc-book
 TEXT_PRIV_VARIANTS			+= flops06-ruler-paper flops06-ruler \
 								popl07-explimpl \
-								hw06-impred \
+								hw06-impred esop07-impred esop07-impred-tr \
 								truu-explimpl truu-ruler \
 								phd-paper phd-draft phd-tst phd \
 								ehc-book-tst \
-								scratch poster \
+								scratch scratch2 \
+								poster \
 								slides-ruler slides-ruler-long \
 								slides-explimpl slides-explimpl-fpnl slides-overview
 
@@ -28,7 +29,7 @@ TEXT_SUBS					+= AGMiniPrimer StoryIntro StoryEH1 StoryEH2 StoryAFP Scratch \
 # 4	: popl07-explimpl, truu-explimpl
 # 5	: hw06-impred
 # 6	: afp (will be obsolete)
-# 7	: scratch
+# 7	: scratch (book format)
 # 8	: slides afp
 # 9	: slides explimpl: base (share)
 # 10: future
@@ -41,6 +42,7 @@ TEXT_SUBS					+= AGMiniPrimer StoryIntro StoryEH1 StoryEH2 StoryAFP Scratch \
 # 17: slides overview
 # 18: slides: base (share)
 # 19: ruler doc
+# 77: scratch (article format)
 
-TEXT_SHUFFLE_ORDER			+= 1 < 2, 1 < 3, 1 < 4, 1 < 5, 1 < 6, 1 < 7, 1 < 8, 18 < 9, 1 < 10, 1 < 11, 1 < 13, 18 < 14, 9 < 15, 9 < 16, 18 < 17, 1 < 18, 1 < 19
+TEXT_SHUFFLE_ORDER			+= 1 < 2, 1 < 3, 1 < 4, 1 < 5, 1 < 6, 1 < 7, 1 < 77, 1 < 8, 18 < 9, 1 < 10, 1 < 11, 1 < 13, 18 < 14, 9 < 15, 9 < 16, 18 < 17, 1 < 18, 1 < 19
 
