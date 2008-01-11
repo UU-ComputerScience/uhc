c683c603a880b22f69ff21b89c7e11caf0082c04
JohnVanSchie <JohnVanSchie@unknown>
2008-01-11 16:34:48 +0000

    Added an easy way to run the no-fib suite with both GHC and EHC by using a shellscript.   * It can run either a suite (real/spectral/imaginary), all suites or one specific benchmark   * It can compile with either EHC or GHC   * It can run 3 types for each benchmark, slow, normal and fast   * Diffing against exepected output is future work (tonight)

--- test/nofib/imaginary/tak/Main.hs
+++ test/nofib/imaginary/tak/Main.hs
@@ -3,24 +3,10 @@
  - Original code by ???
  - Changes to be compiled by EHC: John van Schie
  -}
-
-foreign import ccall "primSubInt" (-)   :: Int -> Int -> Int
-foreign import ccall "primLtInt"  (<)   :: Int -> Int -> Bool
-
-data Bool = False | True
-
-not :: Bool -> Bool
-not x =
-  if x then False else True
-
 tak :: Int -> Int -> Int -> Int
 tak x y z = if not(y < x) then z
        else tak (tak (x-1) y z)
 		(tak (y-1) z x)
 		(tak (z-1) x y)
 
-xs = 33
-ys = 17
-zs = 8
-
-main = tak xs ys zs
+main = <PRINT_INT> tak <ARG1> <ARG2> <ARG3>
