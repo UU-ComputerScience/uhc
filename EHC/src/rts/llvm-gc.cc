276f4741e91b7c906742aca4c3742004872bffda
JohnVanSchie <JohnVanSchie@unknown>
2008-02-19 17:48:46 +0000

    * Changed the LLVMType data type by factor out the different width integer constructors. They are replaced by the |LLVMInt Int| constructor and constants are added for easy referencing often used integer types (i64, i32, i1)
    * Add comments to the LLVM interface file (src/grinc/LLVM.cag) and the LLVM AST (src/grinc/LLVM/AbsSyn.cag). This is work in progress.

--- EHC/src/rts/llvm-gc.cc
+++ EHC/src/rts/llvm-gc.cc
@@ -4,25 +4,20 @@
 #include "config.h"
 #include "gc.h"
 
-void llvmgc_init()
+inline void llvmgc_init()
 {
   #if USE_BOEHM_GC
-  int dummy;
-  uintptr_t stack_bottom = (uintptr_t)&dummy;
-  //stack_bottom += 4095;
-  //stack_bottom &= ~4095;
-  GC_stackbottom = (char*)stack_bottom;
   GC_INIT();
   #endif
 }
 
-void* llvmgc_malloc( size_t nbytes )
+inline void* llvmgc_malloc( size_t nbytes )
 {
 
   return GC_MALLOC( nbytes );
 }
 
-void* llvmgc_malloc_uncollectable( size_t nbytes )
+inline void* llvmgc_malloc_uncollectable( size_t nbytes )
 {
 
   return GC_MALLOC_UNCOLLECTABLE( nbytes );
