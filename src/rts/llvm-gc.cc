8a233dcea6a0d51d25b23e6da1b6c4dc55ddc83c
JohnVanSchie <JohnVanSchie@unknown>
2008-03-05 16:01:26 +0000

    * Print memory statistics to standard error at the end of the program.
    The numbers collected shocked me. Programs from the imaginary suite, allocate more then 100MiB of bytes with calls to malloc, some even more than two gigabytes! These 
    numbers are obtained from the garbage collector but counting in llvm-gc.cc yields numbers that are about equal.

--- src/rts/llvm-gc.cc
+++ src/rts/llvm-gc.cc
@@ -1,5 +1,6 @@
 %%[8
 #include <inttypes.h>
+#include <stdio.h>
 
 #include "config.h"
 #include "gc.h"
@@ -13,14 +14,22 @@ inline void llvmgc_init()
 
 inline void* llvmgc_malloc( size_t nbytes )
 {
-
   return GC_MALLOC( nbytes );
 }
 
 inline void* llvmgc_malloc_uncollectable( size_t nbytes )
 {
-
   return GC_MALLOC_UNCOLLECTABLE( nbytes );
+}
+
+inline void llvmc_print_statistics( )
+{
+  #if USE_BOEHM_GC
+    fprintf( stderr, "-----Memory usage statistics-----\n" );
+    fprintf( stderr, "Bytes on the heap:                %5zu MiB\n", GC_get_heap_size()   / (1024 * 1024) );
+    fprintf( stderr, "Free bytes on the heap:           %5zu MiB\n", GC_get_free_bytes()  / (1024 * 1024) );
+    fprintf( stderr, "Total bytes allocated by program: %5zu MiB\n", GC_get_total_bytes() / (1024 * 1024) );
+  #endif
 }
 
 %%]
