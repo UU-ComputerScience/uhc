a5d0a978860a106b8cd1cfdb41ed412affb18a86
JohnVanSchie <JohnVanSchie@unknown>
2008-03-13 14:57:34 +0000

    * String handling in LLVM backend fixed * Removed inlining of llvm-gc functions * Call llvmgc_init at begin of code * Tried to add profiling to the program by using two clock_t sources. Although this works for percentages (knowing how much of the time is spent on allocation and GC time compared to the user code) it horribly slows the program down (from 24s to 1.50s). Thus I changed it to one clock initialized when the program starts and one if it finishes to print the bytes allocated per second stat. Tomorrow look into a stats version of boehmgc so that we can see how much time is spend in allocation and how much for garbage collection.

--- EHC/src/rts/llvm-gc.cc
+++ EHC/src/rts/llvm-gc.cc
@@ -1,35 +1,76 @@
 %%[8
 #include <inttypes.h>
 #include <stdio.h>
+#include <locale.h>
 
 #include "config.h"
 #include "gc.h"
+#include "timing.h"
 
-inline void llvmgc_init()
+void llvmgc_init()
 {
   #if USE_BOEHM_GC
-  GC_INIT();
-  #endif
+	// Format output by local settings of the machine.
+	// 
+	setlocale (LC_ALL, "");
+
+	// Initialize the GC, needed on a Mac, on other platforms
+	// it translates to an no-op.
+	//
+	GC_INIT();
+
+	#if GC_TIMING
+	    // Begin the timing of the program
+    	//
+		timing_initialize();
+	#endif /* GC_TIMING */	
+  #endif /* USE_BOEHM_GC */
 }
 
-inline void* llvmgc_malloc( size_t nbytes )
+void* llvmgc_malloc( size_t nbytes )
 {
-  return GC_MALLOC( nbytes );
+  #if GC_TIMING
+  	//switch_to_gc();
+  	void* ret = GC_MALLOC( nbytes );
+  	//switch_to_user_code();
+  	return ret;
+  #else
+  	return GC_MALLOC( nbytes );
+  #endif /* GC_TIMING */
 }
 
-inline void* llvmgc_malloc_uncollectable( size_t nbytes )
+void* llvmgc_malloc_uncollectable( size_t nbytes )
 {
-  return GC_MALLOC_UNCOLLECTABLE( nbytes );
+  #if GC_TIMING
+  	//switch_to_gc();
+  	void* ret = GC_MALLOC_UNCOLLECTABLE( nbytes );
+  	//switch_to_user_code();
+  	return ret;
+  #else
+  	return GC_MALLOC_UNCOLLECTABLE( nbytes );
+  #endif /* GC_TIMING */
 }
 
-inline void llvmc_print_statistics( )
+void llvmc_print_statistics( )
 {
-  #if USE_BOEHM_GC
+  #if USE_BOEHM_GC 
+  	#if GC_TIMING
+  		timing_finalize();
+	    fprintf( stderr, "-----Timing statistics-----\n" );
+	    fprintf( stderr, "User code time:                   %4.3f seconds\n", prog_time );
+	    fprintf( stderr, "Garbage collector time:           %4.3f seconds\n", gc_time ); 
+	    fprintf( stderr, "Total:                            %4.3f seconds\n", prog_time + gc_time );
+	#endif /* GC_TIMING */
+	
     fprintf( stderr, "-----Memory usage statistics-----\n" );
-    fprintf( stderr, "Bytes on the heap:                %5zu MiB\n", GC_get_heap_size()   / (1024 * 1024) );
-    fprintf( stderr, "Free bytes on the heap:           %5zu MiB\n", GC_get_free_bytes()  / (1024 * 1024) );
-    fprintf( stderr, "Total bytes allocated by program: %5zu MiB\n", GC_get_total_bytes() / (1024 * 1024) );
-  #endif
+    fprintf( stderr, "Bytes on the heap:                %'15zu bytes\n", GC_get_heap_size()  );
+    fprintf( stderr, "Free bytes on the heap:           %'15zu bytes\n", GC_get_free_bytes() );
+    fprintf( stderr, "Total bytes allocated by program: %'15zu bytes\n", GC_get_total_bytes());
+    
+    #if GC_TIMING
+    	fprintf( stderr, "Bytes allocated per second:       %'15.0f bytes/s\n"
+           	   , ((double) GC_get_total_bytes()) / (prog_time + gc_time) );
+    #endif /* GC_TIMING */
+  #endif /* USE_BOEHM_GC */
 }
-
 %%]
