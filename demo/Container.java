663079c535cb4684ece5d7713a33b3a71b3d6428
sfvisser <sfvisser@unknown>
2009-01-14 10:26:14 +0000

    Finished.

--- demo/Container.java
+++ demo/Container.java
@@ -3,4 +3,14 @@ class Container {
   public Object[] payload;
   public int intVal;
   public String stringVal;
+
+  static void alloc (int size)
+  {
+    Container c = new Container();
+    c.payload = new Object[size - 1];
+    c.payload[3] = c;
+  }
+
 }
+
+
