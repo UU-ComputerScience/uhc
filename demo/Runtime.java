d583b2f46a5ba4e221fea6f19a216635bd017f10
sfvisser <sfvisser@unknown>
2009-01-14 13:22:54 +0000

    Updated runtime.

--- demo/Runtime.java
+++ demo/Runtime.java
@@ -1,18 +1,25 @@
 class Runtime
 {
-  public static Object[] RP = new Object[256];
-
   public static class Container
   {
     public int tag;
     public Object[] payload;
     public int intVal;
     public String stringVal;
+
+    public Container (int tag, Object[] payload)
+    {
+      this.tag     = tag;
+      this.payload = payload;
+    }
   }
+
+  public static Object[]   RP = new Object[256];
+  public static Container CRP = new Container(0, Runtime.RP);
 
   static void test (int size)
   {
-    Container c = new Container();
+    Container c = new Container(0, null);
     c.payload = new Object[size - 1];
     c.payload[3] = c;
     System.out.println(c.payload[2]);
