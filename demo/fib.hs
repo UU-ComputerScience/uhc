0334e3c528f75ca27dca760bf5d71a8ff02913d7
chriseidhof <chris@eidhof.nl>
2009-01-21 17:25:50 +0000

    Now some unboxing, too

--- demo/fib.hs
+++ demo/fib.hs
@@ -4,7 +4,7 @@ data List a = Nil | Cons a (List a)
 -- TODO: should be jvmcall
 foreign import ccall "primAddInt" (+)  :: Int -> Int -> Int
 
-mylist = Cons 1 (Cons 2 (Cons 3 (Cons 4 Nil)))
+mylist = Cons 83 (Cons 2 (Cons 3 (Cons 4 Nil)))
 
 sum Nil         = 0
 sum (Cons x xs) = x + sum xs
@@ -16,6 +16,9 @@ last (Cons x xs)  = last xs
 head (Cons x _) = x
 head Nil        = 13
 
+tail Nil = Nil
+tail (Cons _ xs) = xs
+
 test = \(T a b) -> b
 
-main = 40 + 2
+main = 17 + head mylist
