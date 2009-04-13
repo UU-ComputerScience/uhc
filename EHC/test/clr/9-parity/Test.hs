9fcd6d20f4037b9e724c81dd7ee4e3edabf9eff1
tomlokhorst <tom@lokhorst.eu>
2009-04-13 09:54:32 +0000

    Patched CLR backend so that it compiles again. Due to changes in the HPT analysis (I'm guessing), some GRIN transformations are broken.
    DropUnreachableBindings and DropUnusedExpr don't work anymore.
    
    Steps to reproduce:
    ./configure --enable-clr
    make 8/ehclibs
    install/8/bin/ehc -tclr test/clr/1-add/Test.hs --dump-grin-stages=yes
    
    This will stop compiling at Test-134-reachable.grin, because this is the first tranformation to fail.

--- EHC/test/clr/9-parity/Test.hs
+++ EHC/test/clr/9-parity/Test.hs
@@ -1,5 +1,13 @@
 module Main where
 
+infixr 0  $, $!, `seq`
+
+seq :: forall a b . a -> b -> b
+x `seq` y = letstrict x' = x in y
+
+($!) :: (a -> b) -> a -> b
+f $! x                = x `seq` f x
+
 data Bool = False | True
 
 foreign import ccall "primSubInt" (-)  :: Int -> Int -> Int
@@ -7,11 +15,11 @@ foreign import ccall "primEqInt" (==) :: Int -> Int -> Bool
 
 even :: Int -> Bool
 even x | x == 0 = True
-       | True   = odd (x - 1)
+       | True   = odd $! x - 1
 
 odd :: Int -> Bool
 odd x | x == 0 = False
-      | True   = even (x - 1)
+      | True   = even $! x - 1
 
 main = even 100000000
 
