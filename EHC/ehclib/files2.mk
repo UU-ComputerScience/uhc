c15231eb245a4f68bb24820e1488269c875a39ae
atzedijkstra <atzedijkstra@gmail.com>
2009-12-18 15:14:10 +0000

    oops, left some debugging on

--- EHC/ehclib/files2.mk
+++ EHC/ehclib/files2.mk
@@ -112,9 +112,9 @@ EHCLIB_GHCSYNC_FROZEN_DRV_ARCH			:= $(EHCLIB_BLD_SYNC_PREFIX)$(EHCLIB_GHCSYNC_FR
 # ehclib targets
 ###########################################################################################
 
-#EHCLIB_DEBUG_OPTS						=
+EHCLIB_DEBUG_OPTS						=
 #EHCLIB_DEBUG_OPTS						= -v3
-EHCLIB_DEBUG_OPTS						= -v4
+#EHCLIB_DEBUG_OPTS						= -v4
 #EHCLIB_DEBUG_OPTS						= -v4 --debug-stopat-hi-error=1 
 #EHCLIB_DEBUG_OPTS						= --dump-core-stages=1 --dump-grin-stages=1 --gen-trace=1 --gen-cmt=1
 #EHCLIB_DEBUG_OPTS						= --dump-core-stages=1 --dump-grin-stages=1 --gen-trace=1 --gen-cmt=1 --priv=1
