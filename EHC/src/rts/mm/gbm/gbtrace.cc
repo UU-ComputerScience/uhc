609b0f893e51d43616eb7184ef6c8dee1ca817f9
JeroenFokker <J.D.Fokker@uu.nl>
2010-04-28 11:02:43 +0000

    (v2k!) Thorough documentation of trace functions for garbage collection (BC backend)

--- EHC/src/rts/mm/gbm/gbtrace.cc
+++ EHC/src/rts/mm/gbm/gbtrace.cc
@@ -10,177 +10,154 @@ Trace a GBM object, knowing its structure etc.
 %%]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM internal defs
+%%% Auxiliary function
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%[8
-// is managed by GC
 static inline Bool mm_trace_GBM_CanTrace( Word obj, MM_Collector* collector ) {
-	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_CanTrace obj=%x\n",obj);}) ;
+
+    // This function decides whether obj is a traceable object,
+    // that is: whether it is indeed a pointer (and not an unboxed integer),
+    // and if so, whether it is in the GC-administered address space.
+
 	// TODO: querying via collector->isInCollectedSpace
+
 	Bool res = GB_Word_IsPtr( obj ) && mm_Spaces_AddressIsGCManagedBySpace( obj, collector->collectedSpace ) ;
-#	if TRACE
-		if ( ! res ) {
-			// IF_GB_TR_ON(3,{printf("mm_trace_GBM_CanTrace NOT: x=%x, space(x)=%p, collspace=%p\n", obj, mm_Spaces_GetSpaceForAddress( obj ), collector->collectedSpace );}) ;
-		}
-#	endif
-	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_CanTrace B\n");}) ;
+
+#if TRACE
+	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_CanTrace obj=%x\n",obj);}) ;
+	if (! res ) {
+		// IF_GB_TR_ON(3,{printf("mm_trace_GBM_CanTrace NOT: x=%x, space(x)=%p, collspace=%p\n", obj, mm_Spaces_GetSpaceForAddress( obj ), collector->collectedSpace );}) ;
+	}
+#endif
+
 	return res ;
 }
 
-// is traceable, but later
-/*
-static inline Bool mm_trace_GBM_CanScheduleForTrace( Word obj ) {
-	return GB_NH_Fld_NdEv( ((GB_NodePtr)obj)->header ) != GB_NodeNdEv_Fwd ;
-}
-*/
-
-// is a location which is being collected
-/*
-static inline Bool mm_trace_GBM_InCollectableRange( Word obj ) {
-	return GB_NH_Fld_NdEv( ((GB_NodePtr)obj)->header ) != GB_NodeNdEv_Fwd ;
-}
-*/
 %%]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM internal functions
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM internal functions, but still externally visible (because of inlining)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM interface
+%%% MM_Trace implementation for GBM backend
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%[8
 void mm_trace_GBM_Init( MM_Trace* trace, void* traceSupply, MM_Allocator* allocator, MM_Collector* collector ) {
+
+    // allocate a small object...
 	MM_Trace_GBM_Data* tr = mm_malloc_LOF.malloc( sizeof(MM_Trace_GBM_Data) ) ;
-	
+    // ...to keep the traceSupply	
 	tr->traceSupply = (MM_TraceSupply*)traceSupply ;
+	
+	// Store it, and two more fields, in the trace object.
+	trace->data      = (MM_Trace_Data_Priv*)tr ;
 	trace->collector = collector ;
 	trace->allocator = allocator ;
-		
-	trace->data = (MM_Trace_Data_Priv*)tr ;
 }
 
 Bool mm_trace_GBM_CanTraceObject( MM_Trace* trace, Word obj ) {
+    
+    // This function decides whether obj is a traceable object,
+    // that is: whether it is indeed a pointer (and not an unboxed integer),
+    // and if so, whether it is in the GC-administered address space.
+    // The actual work is done by an (inlined) auxiliary function,
+    // because that function is needed anyway because it is sometimes called directly.
+    
 	return mm_trace_GBM_CanTrace( obj, trace->collector ) ;
 }
 
 Word mm_trace_GBM_TraceKnownToBeObject( MM_Trace* trace, Word obj ) {
+    
+    // This function is responisble for copying an object "from-space" to "to-space".
+    // If the object was already copied before, the copy is returnd.
+    // If the object was not yet copied, it is done now:
+    // - a new object is allocated
+    // - its payload is copied
+    // - the pointers in the payload a queued for copying as well
+    // The original object is overwritten with a forwarding node.
+    // The new object is returned.
+    
 	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject obj=%x obj->header=%x\n",obj,((GB_NodePtr)obj)->header);}) ;
 	
-	GB_NodeHeader h = ((GB_NodePtr)obj)->header ;
-	
-	// skip indirection & forwarding nodes until we are at a real node;
-	// we do not bother repeating this for other indirections as there will in general not be many
-	/**/
-	for ( ; gb_NodeHeader_IsIndirection(h) ; ) {
+	GB_NodeHeader h;
+
+    // If the object is an indirection node, follow the chain of indirections
+
+	for (h=((GB_NodePtr)obj)->header ; gb_NodeHeader_IsIndirection(h) ; h=((GB_NodePtr)obj)->header) {
 		IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject ind1 obj=%x(space=%p) -> %x(space=%p)\n",obj, mm_Spaces_GetSpaceForAddress( obj ),((GB_NodePtr)obj)->content.fields[0], mm_Spaces_GetSpaceForAddress( ((GB_NodePtr)obj)->content.fields[0] ));}) ;
+		
+		// Follow the indirection pointer, effectively removing the indirection node
 		obj = ((GB_NodePtr)obj)->content.fields[0] ;
-		if ( mm_trace_GBM_CanTraceObject( trace, obj ) ) {
-			h = ((GB_NodePtr)obj)->header ;
-		} else {
+		
+		if ( ! mm_trace_GBM_CanTraceObject( trace, obj ) ) 
+		{   // The node was appearantly a forwarding node, indicating that the node was copied before, 
+		    // so we can just return that and we're done
 			return obj ;
 		}
 	}
-	/**/
 
-	/*
-	while ( GB_NH_Fld_NdEv(h) != GB_NodeNdEv_No ) {
-		Bool followFurther ;
-		switch ( GB_NH_Fld_NdEv(h) ) {
-			case GB_NodeNdEv_Yes :
-				if ( GB_NH_Fld_TagCat(h) == GB_NodeTagCat_Ind ) {
-					IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject ind2 obj=%x -> %x\n",obj,((GB_NodePtr)obj)->content.fields[0]);}) ;
-					obj = ((GB_NodePtr)obj)->content.fields[0] ;
-					followFurther = True ;
-				} else {
-					followFurther = False ;
-				}
-				break ;
-			case GB_NodeNdEv_Fwd :
-				IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject fwd1 obj=%x -> %x\n",obj,((GB_NodePtr)obj)->content.fields[0]);}) ;
-				obj = ((GB_NodePtr)obj)->content.fields[0] ;
-				followFurther = True ;
-				break ;
-			default :
-				followFurther = False ;
-				break ;
-		}
-		if ( followFurther ) {
-			if ( mm_trace_GBM_CanTraceObject( trace, obj ) ) {
-				h = ((GB_NodePtr)obj)->header ;
-			} else {
-				return obj ;
-			}
-		} else {
-			break ;
-		}
-	}
-	*/
 
-	GB_NodePtr objRepl = (GB_NodePtr)obj ;
-	
+    // Now we are sure that node "obj", with header "h", must be copied.
+	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject BEF COPY obj=%x, h=%x, sz(h)=%x, space=%p\n", obj, h, GB_NH_Fld_Size(h),mm_Spaces_GetSpaceForAddress( obj ));}) ;
+
+    // Find out the size of the new object to be allocated
+
 	Word szWords ;
-%%[[8
 	szWords = GB_NH_Fld_Size(h) ;
-%%][97
+
+%%[[97
 #	if 0 && USE_LTM
 		if ( GB_NH_Fld_Tag(h) == GB_NodeTag_Intl_LTM_mpz ) {
 			// LTM nodes are overallocated, too large, so we only copy what is necessary.
-			szWords = GB_NodeLTMMpzSize(GB_LTM_Int_Used(objRepl)) ;
+			szWords = GB_NodeLTMMpzSize(GB_LTM_Int_Used((GB_NodePtr)obj)) ;
 			GB_NH_SetFld_Size(h,szWords) ;
-		} else {
-			szWords = GB_NH_Fld_Size(h) ;
 		}
-#	else
-		szWords = GB_NH_Fld_Size(h) ;
 #	endif
 %%]]
 
-	MM_Trace_GBM_Data* tr = (MM_Trace_GBM_Data*)trace->data ;
-	MM_Allocator* alc = trace->allocator ;
-	Bool doTrace ; // = flg & MM_Trace_Flg_Trace ;
-	// Bool doCopy  = flg & MM_Trace_Flg_Copy ;
-	
-	// new obj, copy old into new, allocate
-	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject BEF COPY obj=%x, h=%x, sz(h)=%x, space=%p\n", obj, h, GB_NH_Fld_Size(h),mm_Spaces_GetSpaceForAddress( obj ));}) ;
-	objRepl = (GB_NodePtr)( alc->alloc( alc, szWords << Word_SizeInBytes_Log, 0 ) ) ;
-	IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject AFT COPY obj=%x, h=%x, sz(h)=%x, objRepl=%p, space=%p\n", obj, h, GB_NH_Fld_Size(h),objRepl, mm_Spaces_GetSpaceForAddress( (Word)objRepl ));}) ;
-	// copy header
-	objRepl->header = h ;
 
-	// copy fields
-	Word* fieldTo = objRepl->content.fields ;
-	Word* fieldFr = ((GB_NodePtr)obj)->content.fields ;
+    // Allocate the new object
+	MM_Allocator *allocator  =  trace->allocator ;
+    GB_NodePtr objRepl;
+	objRepl = (GB_NodePtr)( allocator->alloc( allocator, szWords << Word_SizeInBytes_Log, 0 ) ) ;    // alloc wants the size in bytes, so multiply the szWords by the Word_Size
+
+
+    // Initialize the new object
+	objRepl->header = h ;
+	Word* fieldTo   =          objRepl ->content.fields ;
+	Word* fieldFrom = ((GB_NodePtr)obj)->content.fields ;
 	Word sz = szWords ;
 	for ( sz-- ; sz > 0 ; sz-- ) {
-		*(fieldTo++) = *(fieldFr++) ;
+		*(fieldTo++) = *(fieldFrom++) ;
 	}
 
-	// forward old obj to new
+	IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject AFT COPY obj=%x, h=%x, sz(h)=%x, objRepl=%p, space=%p\n", obj, h, GB_NH_Fld_Size(h),objRepl, mm_Spaces_GetSpaceForAddress( (Word)objRepl ));}) ;
+
+
+    // Overwrite the original object with a forwarding node, which points to the new object
+    // The forwarding node has a special tag
 	// ((GB_NodePtr)obj)->header = GB_NH_SetFld_NdEv(h,GB_NodeNdEv_Fwd) ;
-	((GB_NodePtr)obj)->header= GB_MkHeader(GB_NH_Fld_Size(h),GB_NodeNdEv_Yes,GB_NodeTagCat_Ind,0) ;
-	((GB_NodePtr)obj)->content.fields[0] = (Word)objRepl ;
 	// ((GB_NodePtr)obj)->header = GB_NH_SetFld_NdEv((Word)objRepl,GB_NodeNdEv_Fwd) ;
+	((GB_NodePtr)obj)->header= GB_MkHeader(GB_NH_Fld_Size(h),GB_NodeNdEv_Yes,GB_NodeTagCat_Ind,0) ;
+
+	// The forwarding node has a single-word payload, which is the pointer to the new object
+	((GB_NodePtr)obj)->content.fields[0] = (Word)objRepl ;
 	
-	
+
+    // Queue the payload of the freshly copied object to be processed as well
+
 %%[[95
-	doTrace = gb_NH_HasTraceableFields( h ) ;
+	if (gb_NH_HasTraceableFields( h ))
 %%]]
-	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject TRACE obj=%x, objRepl=%x, doTrace=%x, ", obj, objRepl, doTrace); gb_prWord( (Word)objRepl ) ; printf("\n") ;}) ;
-	if ( doTrace ) {
+    {
+        // IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject TRACE obj=%x, objRepl=%x", obj, objRepl); gb_prWord( (Word)objRepl ) ; printf("\n") ;}) ;
 		gb_assert_IsNotDangling_Node( objRepl, "mm_trace_GBM_TraceKnownToBeObject" ) ;
-		if ( True ) {
-			tr->traceSupply->pushWork( tr->traceSupply, (Word*)objRepl, szWords, alc->lastAllocFragment(alc) ) ;
-		} else {
-			mm_trace_GBM_TraceObjectPayload( trace, (Word)objRepl ) ;
-			// mm_trace_GBM_TraceObjects( trace, objRepl->content.fields, szWords ) ;
-		}
+
+        // New strategy: queue the payload to be processed
+        MM_TraceSupply* traceSupply = ((MM_Trace_GBM_Data*)trace->data)->traceSupply;
+		traceSupply->pushWork( traceSupply, (Word*)objRepl, szWords, allocator->lastAllocFragment(allocator) ) ;
+
+        // Obsolete strategy: recurse processing on the payload
+		// mm_trace_GBM_TraceObjectPayload( trace, (Word)objRepl ) ;
 	}
 
 	// IF_GB_TR_ON(3,{printf("mm_trace_GBM_TraceKnownToBeObject B\n");}) ;
@@ -192,6 +169,9 @@ Word mm_trace_GBM_TraceKnownToBeObject( MM_Trace* trace, Word obj ) {
 
 %%[8
 void mm_trace_GBM_TraceObjectPayload( MM_Trace* trace, Word obj ) {
+    
+    // Obsolete(?) function that recursively processes the payload of an object
+    
 	WPtr payload    = ((GB_NodePtr)obj)->content.fields ;
 	WPtr payloadEnd = payload + GB_Node_NrFlds( (GB_NodePtr)obj ) ;
 	for ( ; payload < payloadEnd ; payload++ ) {
@@ -202,15 +182,6 @@ void mm_trace_GBM_TraceObjectPayload( MM_Trace* trace, Word obj ) {
 }
 %%]
 
-%%[8
-%%]
-void mm_trace_GBM_TraceObjects( MM_Trace* trace, Word* objs, Word nrObjs ) {
-	for ( ; nrObjs > 0 ; nrObjs--, objs++ ) {
-		if ( mm_trace_GBM_CanTrace( *objs, trace->collector ) ) {
-			*objs = mm_trace_GBM_TraceKnownToBeObject( trace, *objs ) ;
-		}
-	}
-}
 
 %%[8
 Word mm_trace_GBM_EnsureNoIndirections( MM_Trace* trace, Word obj ) {
@@ -218,8 +189,23 @@ Word mm_trace_GBM_EnsureNoIndirections( MM_Trace* trace, Word obj ) {
 }
 %%]
 
+%%[8
+#ifdef TRACE
+void mm_trace_GBM_Dump( MM_Trace* trace ) {
+}
+#endif
+%%]
+
+%%[8
+#ifdef TRACE
+void mm_trace_GBM_Test() {
+}
+#endif
+%%]
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM interface object
+%%% MM_Trace interface object
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%[8
@@ -233,32 +219,8 @@ MM_Trace mm_trace_GBM =
 	, &mm_trace_GBM_CanTraceObject
 	, &mm_trace_GBM_TraceKnownToBeObject
 	, &mm_trace_GBM_TraceObjectPayload
-	// , &mm_trace_GBM_TraceObjects
-	, &mm_trace_GBM_ObjectSize
-	, &mm_trace_GBM_HasTraceableWords
+	, &mm_trace_GBM_ObjectSize               // defined in h-file to make it inlineable
+	, &mm_trace_GBM_HasTraceableWords        // defined in h-file to make it inlineable
 	, &mm_trace_GBM_EnsureNoIndirections
 	} ;
 %%]
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM dump
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%[8
-#ifdef TRACE
-void mm_trace_GBM_Dump( MM_Trace* trace ) {
-}
-#endif
-%%]
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% GBM test
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%[8
-#ifdef TRACE
-void mm_trace_GBM_Test() {
-}
-#endif
-%%]
-
