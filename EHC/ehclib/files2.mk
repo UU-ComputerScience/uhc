d9d6bf7efb70e4112a06b7a46a826bb6fe991488
atzedijkstra <atzedijkstra@gmail.com>
2012-03-07 10:52:02 +0000

    oops, debugging flags forgotten to remove

--- EHC/ehclib/files2.mk
+++ EHC/ehclib/files2.mk
@@ -177,7 +177,7 @@ EHCLIB_DIST_FILES						:= $(EHCLIB_ALL_SRC) $(EHCLIB_MKF)
 #EHCLIB_BASE_OPTS						= 
 EHCLIB_BASE_OPTS						= -O2
 
-EHCLIB_DEBUG_OPTS						= -v4
+EHCLIB_DEBUG_OPTS						=
 #EHCLIB_DEBUG_OPTS						= --gen-trace=1
 #EHCLIB_DEBUG_OPTS						= --gen-trace=1 --dump-core-stages=1
 #EHCLIB_DEBUG_OPTS						= --dump-core-stages=1
