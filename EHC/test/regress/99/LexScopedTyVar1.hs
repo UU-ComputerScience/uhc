3bee6e389e7f4716035b64bab38d7db33c45796b
Atze Dijkstra <atzedijkstra@gmail.com>
2012-09-06 20:15:21 +0200

    fix of propagation of scoped type variable on result of function

--- EHC/test/regress/99/LexScopedTyVar1.hs
+++ EHC/test/regress/99/LexScopedTyVar1.hs
@@ -1,93 +1,25 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, FlexibleContexts, UndecidableInstances, OverlappingInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 {- ----------------------------------------------------------------------------------------
-   what    : lexically scoped tyvar for instance
-   expected: ok, see comment for the complex Sup inst for depth 2
+   what    : lexically scoped tyvar of function result
+   expected: ok
 ---------------------------------------------------------------------------------------- -}
 
 module LexScopedTyVar1 where
 
-import Data.Maybe
-import Data.Dynamic
+import Unsafe.Coerce
 
-{-
-class Sub a b where
-   upcast :: a -> b
--}
+class GetObjectRef a where
+  getObjectRef :: a -> b
 
-class Sup a b where
-   downcast :: a -> Maybe b
 
-{-
-class Narrow a b where
-   narrow :: a -> b
--}
+cast :: forall a b. GetObjectRef b => a -> Maybe b
+cast a :: Maybe b = 		-- the type of 'b' should be propagated
+  if instanceOf a (getObjectRef (undefined :: b))
+    then Just (unsafeCoerce a)
+    else Nothing
 
-class Widen a b where
-   widen :: a -> Maybe b
-
--- depth 1
-
-{-
-instance Sub (a ()) (a ()) where
-   upcast = id
--}
-
-instance Sup (a ()) (a ()) where
-   downcast = Just
-
--- depth 2
-
-{-
-instance Sub (a (b ())) (a (b ())) where
-   upcast = id
--}
-
-instance Sup (a (b ())) (a (b ())) where
-   downcast = Just
-
-{-
-instance (Sub (a ()) x, Narrow (a (b ())) (a ())) => Sub (a (b ())) x where
-   upcast = upcast . (narrow :: a (b ()) -> a () )
--}
-
--- here ref to scoped tyvar would go wrong because type of 'a' and 'b' would not be propagated correctly
-instance (Sup (a (b ())) (a (b c)), Widen (a ()) (a (b ()))) => Sup (a ()) (a (b c)) where
-   downcast o = case widen o :: Maybe (a (b ())) of
-                  Just r   -> downcast r 
-                  Nothing  -> Nothing
-
-{-
--- depth 3
-
-instance Sub (a (b (c ()))) (a (b (c ()))) where
-   upcast = id
-
-instance Sup (a (b (c ()))) (a (b (c ()))) where
-   downcast = Just
-
-instance (Sub (a (b ())) x, Narrow (a (b (c ()))) (a (b ()))) => Sub (a (b (c ()))) x where
-   upcast = upcast . (narrow :: a (b (c ())) -> a (b ()))
-
-instance (Sup (a (b (c ()))) (a (b (c d))), Widen (a (b ())) (a (b (c ())))) => Sup (a (b ())) (a (b (c d))) where
-   downcast o = case widen o :: Maybe (a (b (c ()))) of
-                  Just r   -> downcast r
-                  Nothing  -> Nothing
-
--- depth 4
-instance Sub (a (b (c (d ())))) (a (b (c (d ())))) where
-   upcast = id
-
-instance Sup (a (b (c (d ())))) (a (b (c (d ())))) where
-   downcast = Just
-
-instance (Sub (a (b (c ()))) x, Narrow (a (b (c (d ())))) (a (b (c ())))) => Sub (a (b (c (d ())))) x where
-   upcast = upcast . (narrow :: a (b (c (d ()))) -> a (b (c ())))
-
-instance (Sup (a (b (c (d ())))) (a (b (c (d e)))), Widen (a (b (c ()))) (a (b (c (d ()))))) => Sup (a (b (c ()))) (a (b (c (d e)))) where
-   downcast o = case widen o :: Maybe (a (b (c (d ())))) of
-                  Just r   -> downcast r
-                  Nothing  -> Nothing
--}
+instanceOf :: a -> b -> Bool
+instanceOf = undefined
 
 main = return ()
